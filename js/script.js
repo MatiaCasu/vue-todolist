120d8b5675e667ea5726d5562e8479b66911a272
Matia <matia.casu@gmail.com>
2021-03-26 15:26:32 +0100

    Rimossi dall'array elementi con indice relativo al click

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* Rifare l'esercizio della to do list come fatto assieme in classe:
+/*
 - stampare in pagina un item per ogni elemento contenuto in un array
 - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
 - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista */
@@ -6,5 +6,11 @@ var app = new Vue({
   el: "#app",
   data: {
     toDoList : ["pulire la stanza", "dare l'acqua alle piante", "fare la lavatrice"]
+  },
+  methods: {
+    deleteItem : function(array, index){
+      array.splice(index, 1);
+    }
   }
+
 });
